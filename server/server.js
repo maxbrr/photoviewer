a2ada748ef9664aa1556fc74bce9c91a543fc0ac
Max Brückner <maxbrckner@gmail.com>
2022-02-21 09:46:09 +0100

    defined directory for static content ./server/public

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const app = express();
 
 const PORT = process.env.PORT || 8080;
 
+app.use(express.static('./public'));
+
 app.listen(PORT, () => {
     console.log(`Server is running in ${process.env.NODE_ENV}mode on port ${PORT}...`);
 })
